a0e45ee32dc52f049a98971530785b8598b96581
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2025-03-13 00:00:14 +0500

    Adds prefix for the assets

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig: NextConfig = {
   /* config options here */
   images: { unoptimized: true },
-  basePath: isProd ? '/kaleem-ullah/' : '',
+  assetPrefix: isProd ? `/public/` : '',
   output: 'export',
 }
 
