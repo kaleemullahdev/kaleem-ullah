c70811485439749a61bb39bdd6a5fea14cc983cb
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2025-03-13 01:09:41 +0500

    Adds next.js export output change

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
 const nextConfig = (phase: string) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER
   const nextConfig = {
+    output: 'export',
     assetPrefix: isDev ? undefined : '/kaleem-ullah',
     images: {
       unoptimized: true,
